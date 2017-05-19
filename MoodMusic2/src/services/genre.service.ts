5bab2f3b350606901c3f622c2da93f4f29d8f5a3
Sander Grandia <seegrand@avans.nl>
2017-05-19 12:58:29 +0200

    Provider fix & Player view

--- MoodMusic2/src/services/genre.service.ts
+++ MoodMusic2/src/services/genre.service.ts
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/map';
   for more info on providers and Angular 2 DI.
 */
 @Injectable()
-export class GenreProvider {
+export class GenreService {
 
   constructor(public http: Http) { }
 
